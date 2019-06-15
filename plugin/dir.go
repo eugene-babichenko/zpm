865200f7369b9cca0444d37272f3d737a2c6b1f0
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2019-06-15 12:05:22 +0300

    plugin/dir: load plugin files when the file name is not the same as the plugin name

--- plugin/dir.go
+++ plugin/dir.go
@@ -25,19 +25,23 @@ func (p Dir) Load() ([]string, error) {
 
 	s[0] = fmt.Sprintf("fpath=($fpath %s)", p.Path)
 
-	entrypoint := filepath.Join(p.Path, filepath.Base(p.Path)+".plugin.zsh")
-	stat, err = os.Stat(entrypoint)
-	if err == nil {
-		if stat.Mode()&os.ModeType == 0 {
-			s = append(s, fmt.Sprintf("source %s", entrypoint))
-		}
+	entrypoints, err := filepath.Glob(filepath.Join(p.Path, "*.plugin.zsh"))
+	if err != nil {
+		return nil, errors.Wrap(err, "while loading directory plugin")
 	}
 
-	theme := filepath.Join(p.Path, filepath.Base(p.Path)+".zsh-theme")
-	stat, err = os.Stat(theme)
-	if err == nil {
-		if stat.Mode()&os.ModeType == 0 {
-			s = append(s, fmt.Sprintf("source %s", theme))
+	themes, err := filepath.Glob(filepath.Join(p.Path, "*.zsh-theme"))
+	if err != nil {
+		return nil, errors.Wrap(err, "while loading directory plugin")
+	}
+	entrypoints = append(entrypoints, themes...)
+
+	for _, entrypoint := range entrypoints {
+		stat, err = os.Stat(entrypoint)
+		if err == nil {
+			if stat.Mode()&os.ModeType == 0 {
+				s = append(s, fmt.Sprintf("source %s", entrypoint))
+			}
 		}
 	}
 
