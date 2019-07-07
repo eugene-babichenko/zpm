555f949c15e5e9c73e221af243fa1c701bd21aeb
Yevhenii Babichenko <eugene.babichenko@gmail.com>
2019-07-07 09:39:26 +0300

    cmd/update: fix update check condition

--- cmd/update.go
+++ cmd/update.go
@@ -10,7 +10,7 @@ import (
 )
 
 func update(name string, pluginInstance plugin.Plugin, onlyMissing bool) {
-	if _, _, err := pluginInstance.Load(); !(plugin.IsNotInstalled(err) && onlyMissing) {
+	if _, _, err := pluginInstance.Load(); plugin.IsNotInstalled(err) && onlyMissing {
 		return
 	}
 
